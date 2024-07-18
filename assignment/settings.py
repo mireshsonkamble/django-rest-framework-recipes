298ee064a905d6b48f40bee45bdfac793463b653
Miresh Mukund Sonkamble <53914850+mireshsonkamble@users.noreply.github.com>
2024-07-18 19:34:12 -0400

    Update settings.py

--- assignment/settings.py
+++ assignment/settings.py
@@ -84,9 +84,16 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-csrftoken',
+    'x-requested-with',
+]
 CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
